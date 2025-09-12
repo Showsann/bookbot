eb039bb45a3a0524cbf3db7bebe9c90de56e1e87
Showsann <jester859@gmail.com>
2025-09-12 01:59:52 -0500

    Sorted List Done

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from stats import get_num_words, get_num_chars, sorted_report
+from stats import print_report
 
 # Main function
 def main():
-	print(f"{get_num_words} words found in the document")
-	print(f"{get_num_chars()}")
-	
+	print(print_report())
 
 main()
--- stats.py
+++ stats.py
@@ -6,8 +6,25 @@ get_num_words = len(get_book_text.split())
 def get_num_chars():
 	counts = {}
 	for char in get_book_text.lower():
-		counts[char] = counts.get(char, 0) + 1
+		if char.isalpha():
+			counts[char] = counts.get(char, 0) + 1
 	return counts
 
+def sort_on(item):
+	return item["num"]
+
 def sorted_report():
-	
+	char_counts = get_num_chars()
+	char_list = [{"char": ch, "num": count} for ch, count in char_counts.items()]
+	char_list.sort(key=sort_on, reverse=True)
+	return char_list
+
+def print_report():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_report():
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
